7e3e91b786f5f13b49f5c92808c4b3356a5bb0cc
Beni Katznelson <beni@katznelson.ca>
2020-09-20 23:03:42 -0400

    fix incorrect state and reconnects

--- src/index.js
+++ src/index.js
@@ -6,98 +6,111 @@ const log = require('simple-node-logger').createSimpleLogger();
 
 const topic = topicPrefix + '/garage/STATUS';
 
-var toggling = false;
-var garageState;
-
 (async () => {
+
+    var toggling = false;
+    var garageState;
+
+    var wsp;
     var connection = new eWeLink({
-        "region" : config.region,
-        "email" : config.email,
-        "password" : config.password,
+        "region": config.region,
+        "email": config.email,
+        "password": config.password,
     });
 
-    // // Get devices and put them into a map keyed by the deviceid
-    // const detailedDevices = await connection.getDevices();
-    // const devices = detailedDevices.reduce(function(map, device) {
-    //     map[device.deviceid] = {
-    //         "online": device.online,
-    //         "switch": device.params.switch,
-    //     };
-    //     return map;
-    // }, {});
-    // log.info("Devices: ", devices);
+// // Get devices and put them into a map keyed by the deviceid
+// const detailedDevices = await connection.getDevices();
+// const devices = detailedDevices.reduce(function(map, device) {
+//     map[device.deviceid] = {
+//         "online": device.online,
+//         "switch": device.params.switch,
+//     };
+//     return map;
+// }, {});
+// log.info("Devices: ", devices);
 
-    const client  = mqtt.connect(config.mqtt);
+    const client = mqtt.connect(config.mqtt);
 
     async function toggle() {
-        toggling = true;
-        log.info("---START TOGGLE---");
-        let startStatus = await connection.getDevicePowerState(config.deviceid);
-        log.info("start state: ", startStatus.state);
-        let startState = startStatus.state;
-        let toggled = false;
-        let endState;
+        try {
+            toggling = true;
+            log.info("---START TOGGLE---");
+            let startStatus = await connection.getDevicePowerState(config.deviceid);
+            log.info("start state: ", startStatus.state);
+            let startState = startStatus.state;
+            let toggled = false;
+            let endState;
 
-        if(startState === 'on') {
-            garageState = 'Closing';
-            endState = 'off';
-            client.publish(topic, garageState, {retain: true});
-            log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
-        }
-        if(startState === 'off') {
-            garageState = 'Opening';
-            endState = 'on';
-            client.publish(topic, garageState, {retain: true});
-            log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
-        }
-
-
-        let currentStatus;
-        while (toggled === false) {
-            await connection.setDevicePowerState(config.deviceid, endState);
-            // It will not swap to closed instantly, need to wait for the garage to close.
-            // Wait 15s to see if the garage closed otherwise loop.
             if (startState === 'on') {
-                log.info("waiting 15s to see if garage closed...");
-                await new Promise(r => setTimeout(r, 15000));
+                garageState = 'Closing';
+                endState = 'off';
+                client.publish(topic, garageState, {retain: true});
+                log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
+            }
+            if (startState === 'off') {
+                garageState = 'Opening';
+                endState = 'on';
+                client.publish(topic, garageState, {retain: true});
+                log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
             }
 
-            currentStatus = await connection.getDevicePowerState(config.deviceid);
-            if (currentStatus.state === endState) {
-                toggled = true;
+
+            let currentStatus;
+            while (toggled === false) {
+                await connection.setDevicePowerState(config.deviceid, endState);
+                // It will not swap to closed instantly, need to wait for the garage to close.
+                // Wait 15s to see if the garage closed otherwise loop.
                 if (startState === 'on') {
-                    garageState = 'Closed';
+                    log.info("waiting 15s to see if garage closed...");
+                    await new Promise(r => setTimeout(r, 15000));
                 }
-                if (startState === 'off') {
-                    garageState = 'Open';
+
+                currentStatus = await connection.getDevicePowerState(config.deviceid);
+                if (currentStatus.state === endState) {
+                    toggled = true;
+                    if (startState === 'on') {
+                        garageState = 'Closed';
+                    }
+                    if (startState === 'off') {
+                        garageState = 'Open';
+                    }
                 }
             }
+
+
+            client.publish(topic, garageState, {retain: true});
+            log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
+
+            log.info("---END TOGGLE---");
+            toggling = false;
+        } catch (e) {
+            log.error('Error toggling');
+            log.error(e);
         }
-
-
-        client.publish(topic, garageState, {retain: true});
-        log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
-
-        log.info("---END TOGGLE---");
-        toggling = false;
     }
 
     async function publishStatus() {
-        let status = await connection.getDevicePowerState(config.deviceid);
-        log.info("Device power status: ", status);
-        let topic = topicPrefix + '/garage/STATUS';
+        try {
+            let status = await connection.getDevicePowerState(config.deviceid);
+            log.info("Device power status: ", status);
+            let topic = topicPrefix + '/garage/STATUS';
 
-        if(status.state === 'off') {
-            garageState = 'Closed';
-        } else if(status.state === 'on') {
-            garageState = 'Open';
+            if (status.state === 'off') {
+                garageState = 'Closed';
+            } else if (status.state === 'on') {
+                garageState = 'Open';
+            }
+
+            // only send message if toggling is false
+            if (toggling === false) {
+                client.publish(topic, garageState, {retain: true});
+                log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
+            }
+        } catch (e) {
+            log.error('Error publishing status');
+            log.error(e);
         }
 
-        // only send message if toggling is false
-        if(toggling === false) {
-            client.publish(topic, garageState, {retain: true});
-            log.info("MQTT MESSAGE SENT - ", "Topic: ", topic, " Message: ", garageState);
-        }
     }
 
     client.on('close', () => {
@@ -115,24 +128,24 @@ var garageState;
         log.info("MQTT Connected on topic: ", topic)
     });
 
-    client.on('message', async(topic, message) => {
+    client.on('message', async (topic, message) => {
         log.info("MQTT MESSAGE RECEIVED - ", "Topic: ", topic, " Message: ", message.toString());
-        if(toggling === false) {
-            if(message.toString() === "Open") {
-                if(garageState === "Open") {
+        if (toggling === false) {
+            if (message.toString() === "Open") {
+                if (garageState === "Open") {
                     log.info("Garage already Open...ignoring command.");
-                } else{
+                } else {
                     log.info("Running Toggle");
                     await toggle();
                 }
-            } else if(message.toString() === "Closed") {
-                if(garageState === "Closed"){
+            } else if (message.toString() === "Closed") {
+                if (garageState === "Closed") {
                     log.info("Garage already Closed...ignoring command.");
                 } else {
                     log.info("Running Toggle");
                     await toggle();
                 }
-            } else if(message.toString() === "Status") {
+            } else if (message.toString() === "Status") {
                 await publishStatus();
             } else {
                 log.error("Unknown Command!");
@@ -142,54 +155,55 @@ var garageState;
         }
     });
 
-    async function websocket() {
-
+    try{
+        log.info('websocket getting credentials');
         await connection.getCredentials()
-            .catch(async event => {
-                log.error("Failed to get eWeLink credentials");
-                log.info('Sleep for 30s');
-                await new Promise(resolve => setTimeout(resolve, 30000));
-                log.info('Restarting websocket connection');
-                await websocket();
-            });
-
-        let wsp = await connection.openWebSocket(async data => {
-            // data is the message from eWeLink
-            if(data !== "pong") {
-                log.info("Websocket: ", data);
-            }
-            //Publish Garage Status
-            if(data.userAgent === "device") {
-                await publishStatus();
-            }
-        })
-            .catch(async event => {
-                log.error("Websocket connection failure");
-                log.info('Sleep for 30s');
-                await new Promise(resolve => setTimeout(resolve, 30000));
-                log.info('Restarting websocket connection');
-                await websocket();
-            });
-
-        wsp.onClose.addListener(
-            async event => {
-                log.error('Websocket connection closed - ', event.reason);
-                log.info('Sleep for 30s');
-                await new Promise(resolve => setTimeout(resolve, 30000));
-                log.info('Restarting websocket connection');
-                await websocket();
-            }
-        );
-
-        log.info('Websocket connected');
-        await publishStatus().catch((e) => {
-                log.error("Unable to publish status to MQTT - ", e)
-            });
+    } catch(e) {
+        log.error('failed getting websocket credentials', e);
+        process.exit();
     }
 
-    await websocket();
+    try {
+        log.info('Opening websocket connection');
+        wsp = await connection.openWebSocket(() => {});
+        log.info('Websocket connection open');
+        log.info('Setting initial garage state');
+        await publishStatus();
+    } catch (e) {
+        log.error('Initial open websocket threw an exception', e);
+        process.exit();
+    }
 
-})()
-    .catch((e) => {
-        log.error("Uncaught exception", e)
-    });
+    wsp.onError.addListener(
+        async event => {
+            log.error('Websocket error occurred - ', event.reason);
+            log.error(event);
+        }
+    );
+
+    wsp.onClose.addListener(
+        async event => {
+            log.error('Websocket connection closed - ', event.reason);
+            while (wsp.isClosed) {
+                try {
+                    log.info('Attempting to re-open websocket connection');
+                    wsp = await connection.openWebSocket(() => {});
+                    log.info('Websocket connection open');
+                    log.info('Setting initial garage state');
+                    await publishStatus();
+                } catch (e) {
+                    log.error('Opening websocket connection threw an exception', event.reason);
+                }
+                if(wsp.isClosed){
+                    log.info('websocket connection failed');
+                    log.info('Sleep for 30s before attempting again');
+                    await new Promise(resolve => setTimeout(resolve, 30000));
+                }
+            }
+        }
+    );
+
+})().catch((e) => {
+    log.error('Global level error');
+    log.error(e);
+});
